ec422348c0a0671c4c76008431f3cacbfb4839e9
q400400200-pixel <q.400400200@gmail.com>
2025-08-25 15:27:29 +0000

    add

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,19 +1,21 @@
+
 #include <stdio.h>
 #include "main.h"
 
 /**
- * print_alphabet - Print alphabet
- *
- * My function
- *
- * Return: Always 0 (Success)
- */
+* print_alphabet - Print alphabet
+*
+* My function
+*
+* Return: Always 0 (Success)
+*/
 
 void print_alphabet(void)
 {
-	int a;
+int a;
 
-	for (a = 0; a < 26; a++)
-		_putchar(97 + a);
-	_putchar('\n');
+for (a = 0; a < 26; a++)
+_putchar(97 + a);
+_putchar('\n');
 }
+
