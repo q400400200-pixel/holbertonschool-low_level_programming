1901d925f9f3349b2f917888eec1df25294eac36
q400400200-pixel <q.400400200@gmail.com>
2025-08-25 14:57:25 +0000

    add

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,14 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - Print a string
+ *
+ * My function: prints _putchar
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -16,6 +20,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
