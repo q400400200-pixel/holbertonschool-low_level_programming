78f662612d66c22241285f91bef0a6643e1dd16d
q400400200-pixel <q.400400200@gmail.com>
2025-08-31 14:22:07 +0000

    add

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9
- *
- * Return: void
- */
+* print_numbers - prints numbers from 0 to 9
+*
+* Return: void
+*/
 void print_numbers(void)
 {
-    char c = '0';
+char c = '0';
 
-    while (c <= '9')
-    {
-        _putchar(c);
-        c++;
-    }
-    _putchar('\n');
+while (c <= '9')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
 }
