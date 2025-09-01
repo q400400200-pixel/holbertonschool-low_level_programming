f636bcdbe5d235511471802eff83adb58921bc61
q400400200-pixel <q.400400200@gmail.com>
2025-09-01 15:16:06 +0000

    add

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer
- * @s: input string
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
  *
  * Return: the integer value
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
-	int found_digit = 0;
+	int i = 0;
+	int sign = 1;
+	unsigned int num = 0;
+	int started = 0;
 
-	/* تخطى أي شيء قبل الأرقام */
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] == '+')
-			sign *= 1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			found_digit = 1;
-			result = result * 10 + (s[i] - '0');
+			started = 1;
+			num = num * 10 + (s[i] - '0');
 		}
-		else if (found_digit)
+		else if (started)
 			break;
 		i++;
 	}
-	return (result * sign);
+
+	return (sign * num);
 }
