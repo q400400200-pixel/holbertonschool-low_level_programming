a5608694104ee2dc0f8b78e6720b3bb34ae3eab5
q400400200-pixel <q.400400200@gmail.com>
2025-09-07 15:41:20 +0000

    add

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>  /* for NULL */
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to search
+ * @c: the character to find
+ *
+ * Return: pointer to first occurrence of c in s, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
