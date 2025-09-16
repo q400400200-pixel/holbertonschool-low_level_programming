c71ee5b530fd2c6265d584deabf6029524717f79
q400400200-pixel <q.400400200@gmail.com>
2025-09-16 12:13:30 +0000

    add

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated memory, or exit(98) if malloc fails
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
+}
