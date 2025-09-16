763fefbb640c03b957fc9d81a933aa1f2734f861
q400400200-pixel <q.400400200@gmail.com>
2025-09-16 14:48:23 +0000

    add

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,27 +1,26 @@
-#ifndef __DOG__H__
-#define __DOG__H__
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - define a new type struct dog
- * dog_t - typedef for struct dog
- *
- * @name: first argument
- * @age: second argument
- * @owner: third argument
- *
- * Description: initialize a struct dog
+ * struct dog - structure for dog information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
-
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
-#endif
+#endif /* DOG_H */
