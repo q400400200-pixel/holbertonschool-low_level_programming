75ceaf5d7378d4f417aa2f63c3d645028061400b
q400400200-pixel <q.400400200@gmai.com>
2025-09-22 14:05:40 +0000

    add

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a given function
+ * @name: the name to print
+ * @f: pointer to a function that takes a char * and returns nothing
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
