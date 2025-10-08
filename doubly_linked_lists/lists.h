8e9d594805032468817ae6a425c5e8ee01262dbb
q400400200-pixel <q.400400200@gmail.com>
2025-10-08 16:25:17 +0000

    add

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,16 +1,19 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef __LISTS__H__
+#define __LISTS__H__
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
- * struct dlistint_s - doubly linked list node
- * @n: integer stored in the node
+ * struct dlistint_s - doubly linked list
+ * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
  *
- * Description: node structure for a doubly linked list project
+ * Description: doubly linked list node structure
+ *
  */
 typedef struct dlistint_s
 {
@@ -19,8 +22,14 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-/* Prototype for your function */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* LISTS_H */
+#endif
