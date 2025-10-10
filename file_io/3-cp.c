9d04996df4865bc6143e5f1f31e61d9761e9f191
q400400200-pixel <q.400400200@gmail.com>
2025-10-10 12:48:51 +0000

    add

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,28 +5,15 @@
 #define BUF_SIZE 1024
 
 /**
- * print_error - prints an error message and exits
- * @code: exit code
- * @msg: format string
- * @arg: filename
- */
-void print_error(int code, const char *msg, const char *arg)
-{
-	dprintf(STDERR_FILENO, msg, arg);
-	exit(code);
-}
-
-/**
  * main - copies the content of a file to another file
  * @argc: argument count
  * @argv: argument vector
- *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, r, w;
-	char buf[BUF_SIZE];
+	char buffer[BUF_SIZE];
 
 	if (argc != 3)
 	{
@@ -36,22 +23,37 @@ int main(int argc, char *argv[])
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		print_error(98, "Error: Can't read from file %s\n", argv[1]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-		print_error(99, "Error: Can't write to %s\n", argv[2]);
-
-	while ((r = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
-		w = write(fd_to, buf, r);
-		if (w == -1 || w != r)
-			print_error(99, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
+		exit(99);
 	}
 
-	/* التحقق بعد الحلقة */
-	if (r == -1)
-		print_error(98, "Error: Can't read from file %s\n", argv[1]);
+	while ((r = read(fd_from, buffer, BUF_SIZE)) != 0)
+	{
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close(fd_from);
+			close(fd_to);
+			exit(98);
+		}
+		w = write(fd_to, buffer, r);
+		if (w == -1 || w != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
 
 	if (close(fd_from) == -1)
 	{
@@ -63,5 +65,6 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
+
 	return (0);
 }
